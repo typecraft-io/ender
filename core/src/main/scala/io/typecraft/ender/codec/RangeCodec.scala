b43974aefb4e4cd51a44f767c2884de4eac43630
EntryPoint <entrypointkr@gmail.com>
2022-04-18 02:16:27 +0900

    fix: range codec

--- core/src/main/scala/io/typecraft/ender/codec/RangeCodec.scala
+++ core/src/main/scala/io/typecraft/ender/codec/RangeCodec.scala
@@ -1,6 +1,10 @@
 package io.typecraft.ender.codec
 
 object RangeCodec {
+  private val toKeyword: String = ":"
+  private val untilKeyword: String = "until"
+  private val stepKeyword: String = "step"
+
   def encode(range: Range): String = {
     val sep = range match {
       case _: Range.Inclusive => ":"
@@ -16,13 +20,19 @@ object RangeCodec {
   }
 
   def decode(s: String): Range = {
-    val toIndex = s.indexOf(":")
-    val sepIndex = if (toIndex >= 0) toIndex else s.indexOf("until")
-    val stepIndex = s.indexOf("step")
+    val toIndex = s.indexOf(toKeyword)
+    val sepIndex = if (toIndex >= 0) toIndex else s.indexOf(untilKeyword)
+    val sepSize = if (toIndex >= 0) toKeyword.length else untilKeyword.length
+    val stepIndex = s.indexOf(stepKeyword)
     val from = s.substring(0, sepIndex).trim
     val to =
-      s.substring(sepIndex + 2, if (stepIndex >= 0) stepIndex else s.length)
-    val step = if (stepIndex >= 0) s.substring(stepIndex, s.length) else ""
+      s.substring(
+        sepIndex + sepSize,
+        if (stepIndex >= 0) stepIndex else s.length
+      ).trim
+    val step =
+      if (stepIndex >= 0) s.substring(stepIndex + stepKeyword.length, s.length).trim
+      else ""
 
     if (toIndex >= 0) {
       Range.inclusive(from.toInt, to.toInt, step.toIntOption.getOrElse(1))
--- core/src/test/scala/io/typecraft/ender/codec/RangeCodecSpec.scala
+++ core/src/test/scala/io/typecraft/ender/codec/RangeCodecSpec.scala
@@ -7,12 +7,12 @@ class RangeCodecSpec extends AnyFlatSpec {
     assertResult("0:1")(RangeCodec.encode(0 to 1))
     assertResult("0 until 1")(RangeCodec.encode(0 until 1))
     assertResult("0:2 step 2")(RangeCodec.encode(Range.inclusive(0, 2, 2)))
-    assertResult("0 until 2 step 2")(RangeCodec.encode(Range(0, 2, 2)))
+    assertResult("0 until 3 step 2")(RangeCodec.encode(Range(0, 3, 2)))
   }
   "Range" should "be decoded from String" in {
     assertResult(0 to 1)(RangeCodec.decode("0:1"))
     assertResult(0 until 1)(RangeCodec.decode("0 until 1"))
     assertResult(Range.inclusive(0, 2, 2))(RangeCodec.decode("0:2 step 2"))
-    assertResult(Range(0, 2, 2))(RangeCodec.decode("0 until 2 step 2"))
+    assertResult(Range(0, 3, 2))(RangeCodec.decode("0 until 3 step 2"))
   }
 }
